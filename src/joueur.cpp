0a53c9a2fc4cab8e15d1072045b76b0afc2f40c6
Marie-anaïs Chabal <marie-anais.chabal@etu.utc.fr>
2023-11-07 11:35:37 +0100

    premier jet pour les fichiers joueur

--- src/joueur.cpp
+++ src/joueur.cpp
@@ -2,22 +2,20 @@
 #include <iostream>
 
 
-Joueur::Joueur(const std::string nom)
-        : pseudo(nom), nombre_couronnes(0), points_prestige_total(0), adversaire(nullptr), droitDeRejouer(false) {
+Joueur::Joueur(const std::string nom): pseudo(nom), nombre_couronnes(0), points_prestige_total(0) {
 
     for (int i = 0; i < 6; i++) {
-        cartes_reservees[i] = CarteJoaillerie();  // Initialisez avec des valeurs par défaut
-        cartes_main[i] = CarteJoaillerie();       // Initialisez avec des valeurs par défaut
-        cartes_noble[i] = CarteNoble();            // Initialisez avec des valeurs par défaut
-        privileges[i] = Privilege();               // Initialisez avec des valeurs par défaut
+        cartes_reservees[i] = CarteJoaillerie();
+        cartes_main[i] = CarteJoaillerie();
+        cartes_noble[i] = CarteNoble();
+        privileges[i] = Privilege();
     }
 
     for (int i = 0; i < 5; i++) {
-        gemmes_bonus[i] = 0;                      // Initialisez avec des valeurs par défaut
-        points_prestige_couleurs[i] = 0;          // Initialisez avec des valeurs par défaut
+        gemmes_bonus[i] = 0;
+        points_prestige_couleurs[i] = 0;
     }
 }
-
 
 int Joueur::getPointsPrestigeTotal() const{
     return points_prestige_total;
@@ -39,39 +37,35 @@ int Joueur:: getPointsPrestigeCouleurs(int index) const {
 }
 
 CarteJoaillerie Joueur::getCarteReservee(int index) const {
-    if (index >= 0 && index < 6) {
+    if (index >= 0 && index <= 3) {
         return cartes_reservees[index];
     }
-    return CarteJoaillerie(); // Retourne une carte vide par défaut
 }
 
 CarteJoaillerie Joueur::getCarteMain(int index) const {
-    if (index >= 0 && index < 6) {
+    if (index >= 0 ) {
         return cartes_main[index];
     }
-    return CarteJoaillerie();
 }
 
 
 CarteNoble Joueur::getCarteNoble(int index) const {
-    if (index >= 0 && index < 6) {
+    if (index >= 0 && index < 3) {
         return cartes_noble[index];
     }
-    return CarteNoble();
 }
 
 int Joueur::getGemmesBonus(int index) const {
-    if (index >= 0 && index < 5) {
+    if (index >= 0 && index < 6) {
         return gemmes_bonus[index];
     }
     return 0;
 }
 
 Privilege Joueur::getPrivilege(int index) const {
-    if (index >= 0 && index < 6) {
+    if (index >= 0 && index < 3) {
         return privileges[index];
     }
-    return Privilege();
 }
 
 void Joueur::ajouterCarteJoaillerie(const CarteJoaillerie& carte) {
@@ -99,8 +93,7 @@ void Joueur::ajouterCarteJoaillerie(const CarteJoaillerie& carte) {
         gemmes_bonus[static_cast<int>(couleur)] += nombreGemmes;
     }
 }
-
-void Joueur::ajouterCarteNoble(const CarteNoble &carte) {
+void Joueur::ajouterCarteNoble(const CarteNoble& carte) {
 
     int nombreCouronnesCarte = carteNoble.getNombreCouronnes();
 
@@ -111,27 +104,30 @@ void Joueur::ajouterCarteNoble(const CarteNoble &carte) {
 
         // Ajouter les points de prestige de la carte à la variable points_prestige_total
         points_prestige_total += carteNoble.getPointsPrestige();
-    } else {
-        throw std::runtime_error("Le joueur doit avoir 3 ou 6 couronnes pour ajouter une carte noble.");
+    }
+    else{
+        throw ("Le joueur doit avoir 3 ou 6 couronnes pour ajouter une carte noble.");
     }
 }
 
 
-void Joueur::ajouterCarteReservee(const CarteJoaillerie &carte) {
+void Joueur::ajouterCarteReservee(const CarteJoaillerie& carte) {
     if (compteur_cartes_reservees < 3) {
         compteur_cartes_reservees++;
-    } else {
-        throw std::runtime_error("Le joueur ne peut pas réserver plus de 3 cartes.");
+    }
+    else {
+        throw ("Le joueur ne peut pas réserver plus de 3 cartes.");
     }
 }
 
-void Joueur::ajouterPrivilege(const Privilege &privilege) {
+void Joueur::ajouterPrivilege(const Privilege& privilege) {
     if (compteur_privileges < 3) {
         compteur_privileges++;
     } else {
-        throw std::runtime_error("Le joueur ne peut pas avoir plus de 3 privilèges.");
+        throw ("Le joueur ne peut pas avoir plus de 3 privilèges.");
     }
 }
+
 
 // Définir l'adversaire
 void Joueur::setAdversaire(Joueur *adv) {
