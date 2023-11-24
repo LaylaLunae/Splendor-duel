16348de737cd6d08a5e61ff451de19d935d2c5a0
Elisa Robeau <elisa.robeau@etu.utc.fr>
2023-11-24 16:54:39 +0000

    Upload New File

---
+++ include/jeu.h
@@ -0,0 +1,53 @@
+#ifndef JEU_JEU_H
+#define JEU_JEU_H
+#include <string>
+
+#include "carteJoaillerie.h"
+#include "carte.h"
+#include "carteNoble.h"
+#include "privilege.h"
+#include "jeton.h"
+#include "plateau.h"
+#include "optionnelle.h"
+#include "pioche.h"
+#include "obligatoire.h"
+#include "joueur.h"
+
+
+class Jeu {
+private:
+    /*struct Handler {
+        Jeu * instance = nullptr;
+        ~Handler() { delete instance; instance = nullptr;}
+    };
+    static Handler handler;*/
+
+    static Jeu* instance;
+
+    Joueur* joueur_actuel;
+
+    Jeu(){};
+    ~Jeu(){};
+    Jeu(const Jeu& j) = delete;
+    Jeu& operator=(const Jeu& j) = delete;
+
+public:
+    static Jeu& getJeu();
+    static void libereJeu();
+
+    Joueur* getJoueurActuel() const {return joueur_actuel;}
+
+    void validationAction();
+
+    //pour l'instant dans public
+    void nouvellePartie();
+    void reprendrePartie(); //faut bdd
+    void verifAnciennePartie(); //faut bdd
+    void verifGagnant();
+    void sauvegarderPartie(); //faut bdd
+    Joueur* vainqueur();
+    void auSuivant();
+    void verifObligationRemplissage();
+};
+
+#endif //JEU_JEU_H
