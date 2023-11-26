884ecd316ac96be7b14796fdaabb113ea546eb9a
amranial <alexandre.amrani@etu.utc.fr>
2023-11-26 15:48:40 +0100

    Affichage plateau v0.1

---
+++ include/jeton.h
@@ -0,0 +1,73 @@
+#ifndef JETON_H
+#define JETON_H
+
+#include <string>
+#include <QWidget>
+#include <QPen>
+#include <QBrush>
+#include <QPushButton>
+
+#include "couleur.h"
+
+enum class JetonType {
+    Or,
+    Gemme,
+};
+
+class Jeton {
+private:
+    JetonType type;
+    Couleur couleur; // Utilisé uniquement lorsque type = Gemme
+    std::string chemin_vers_image;
+public:
+    Jeton(JetonType t, Couleur c = Couleur::rien) : type(t), couleur(c) {}
+    ~Jeton() {}
+
+    void afficher() const;
+    std::string getCouleurString() const;
+    Couleur getCouleur() const;
+    JetonType getType() const { return type; }
+};
+
+
+class VueJeton :public QPushButton {
+Q_OBJECT
+public:
+    VueJeton(const Jeton j, QWidget *parent = nullptr):jeton(&j),QPushButton(parent) {
+        setBackgroundRole(QPalette::Base);
+        setAutoFillBackground(true);
+        setFixedSize(120,200);
+        connect(this,SIGNAL(clicked()),this,SLOT(clickedEvent()));
+        setCheckable(false);
+    };
+    explicit VueJeton(QWidget *parent = nullptr):QPushButton(parent) {
+        setBackgroundRole(QPalette::Base);
+        setAutoFillBackground(true);
+        setFixedSize(120, 200);
+        connect(this, SIGNAL(clicked()), this, SLOT(clickedEvent()));
+        setCheckable(false);
+
+    };
+protected:
+    void paintEvent(QPaintEvent *event) override;
+private:
+    const Jeton* jeton = nullptr;
+    QPen pen;
+    QBrush brush;
+signals:
+    // quand la vude de carte est cliquée, elle émet un signal en transmettant son adresse
+    void jetonClick(VueJeton*);
+public slots:
+private slots:
+    void clickedEvent() { emit jetonClick(this); }
+
+    void setJeton(const Jeton& j) {
+        setCheckable(true);
+        setChecked(false);
+        jeton=&j; update();
+    }
+
+};
+
+
+#endif // JETON_H
---
+++ include/plateau.h
@@ -0,0 +1,107 @@
+//
+// Created by alexa on 24/10/2023.
+//
+
+#ifndef SPLENDOR_PLATEAU_H
+#define SPLENDOR_PLATEAU_H
+
+#include <iostream>
+#include <tuple>
+#include <QGridLayout>
+#include <vector>
+
+#include "privilege.h" // safe
+#include "jeton.h" // safe
+//#include "carteNoble.h"
+
+class CarteNoble;
+
+class PlateauException {
+    std::string info;
+public:
+    PlateauException(const std::string& s): info(s) {}
+    const std::string& get_info() const {return info;}
+};
+
+struct ReponseValidationSelection {
+    const Jeton** jetons;
+    unsigned int nombre;
+};
+
+class Plateau {
+    const Jeton** jetons;
+    const Privilege** privileges;
+    const Jeton** sac;
+    const CarteNoble** cartes_nobles;
+    unsigned int nb_jetons_sac;
+    unsigned int nb_jetons_plateau;
+    unsigned int pointeur_case_libre = 0;
+    unsigned int nb_privileges;
+    unsigned int nb_carte_noble = 4;
+
+
+    const Jeton** selection_courante;
+    int* selection_courante_positions;
+    unsigned int nombre_jetons_dans_selection = 0;
+
+    // Constantes
+    unsigned int nombre_jetons_par_cote_de_plateau = 5;
+    unsigned int nombre_jetons_dans_selection_MAX = 3;
+    unsigned int nb_jetons_sac_MAX = 25;
+    unsigned int nb_privileges_MAX = 3;
+    unsigned int nb_jetons_plateau_MAX = 25;
+
+    bool verificationSelectionPositions();
+
+    friend class VuePlateau;
+    const Jeton* getJeton(unsigned int i) const  {return jetons[i];}
+public:
+
+    // A mettre en privé plus tard :
+    Plateau();
+    //friend class Jeu;
+
+    void remplissagePlateau(bool avecAffichage = false);
+    std::string etatPlateau();
+    void ajouterSac(const Jeton *j);
+
+    void selectionJeton(unsigned int position_x, unsigned int position_y);
+    ReponseValidationSelection* validerSelectionEtPrendreJetons();
+    const Jeton* prendreJeton(unsigned int position_x, unsigned int position_y);
+
+    const Privilege* prendrePrivilege();
+    void donnePrivilege(const Privilege* p);
+    const CarteNoble* prendreCarteNoble(unsigned int numero);
+
+    bool hasJeton(); // Vérifiez s'il y a jeton en plateau
+
+    ~Plateau();
+
+    // Usefull for Optionnelle and Obligatoire
+    unsigned int getNbJetonsPlateau() const { return nb_jetons_plateau; }
+    unsigned int getNbJetonsPlateauMAX() const { return nb_jetons_plateau_MAX; }
+    unsigned int getNbJetonsSac() const { return nb_jetons_sac; }
+    unsigned int getNbPrivilegeMAX() const { return nb_privileges_MAX; }
+    unsigned int getNbPrivileges() const { return nb_privileges; }
+    bool hasJetonOr(); // Vérifier qu'il y a au moins un jeton or sur le plateau
+};
+
+std::tuple<int, int> choisir_jeton();
+
+void testes_pour_plateau();
+
+class VuePlateau : public QWidget {
+Q_OBJECT
+public:
+    explicit VuePlateau(QWidget *parent = nullptr);
+private:
+    Plateau* plateau = nullptr;
+    QGridLayout* layoutBouton;
+    std::vector<VueJeton*> vuesJetons;
+
+private slots:
+    void jetonClick_Plateau(VueJeton*) ;//{ std::cout<<"Clicked!\n"; }
+
+};
+
+#endif //SPLENDOR_PLATEAU_H
