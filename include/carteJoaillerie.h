c925c454d459a46461e2a7f176ecff8514e5cc49
Timo Allais <timo.allais@etu.utc.fr>
2023-11-30 12:59:23 +0100

    Ajout ID carteJoaillerie + fonction joueurPrendPioche

--- include/carteJoaillerie.h
+++ include/carteJoaillerie.h
@@ -18,16 +18,19 @@ private:
     int couronne;  // Le score correspondant à la carte
     std::array<Couleur, 2> pierres; // La couleur que le joueur peut déduire lors de l'achat d'autres cartes à l'avenir.
     std::map<Couleur, int> prix;
+    const int id;
 
 public:
-    CarteJoaillerie(Plateau* p, Joueur* j, int niv, int cour, const std::array<Couleur, 2>& pierres, std::map<Couleur, int> prx, const std::string& chemin, int prestige, const Pouvoir& pvr1, const Pouvoir& pvr2);
+    CarteJoaillerie(Plateau* p, Joueur* j, int niv, int cour, const std::array<Couleur, 2>& pierres, std::map<Couleur, int> prx, const std::string& chemin, int prestige, const Pouvoir& pvr1, const Pouvoir& pvr2, const int id);
     CarteJoaillerie();
     ~CarteJoaillerie();
+
 
     // Specific methods for CarteJoaillerie
     std::map<Couleur, int> getPrix() const;
     int getNiveau() const;
     int getCouronne() const;
+    const int getID() const;
     bool hasPierre() const;
     Couleur getTypePierre() const;
     void setPierreColor(int index, Couleur couleur);
--- src/pioche.cpp
+++ src/pioche.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 //#include "pioche.h"
 #include "../include/jeu.h"
 
@@ -8,15 +10,18 @@ Pioche::Pioche(int np, int mcr, int mcp):numero_pioche(np),
     cartes_dehors = new const CarteJoaillerie*[mcr];
 
     for (int i = 0; i < mcp; ++i) {
-        cartes_dans_pioche[i] = new CarteJoaillerie(); // A modifier avec CarteJoaillerie
+        cartes_dans_pioche[i] = nullptr;
+    }
+    for (int i = 0; i < mcr; ++i) {
+        cartes_dehors[i] = nullptr;
     }
 }
 
 Pioche::~Pioche(){
-    for (int i = 0; i < max_cartes_pioche; ++i) {
+    for (int i = 0; i < max_cartes_pioche; i++) {
         delete cartes_dans_pioche[i];
     }
-    for (int i = 0; i < max_cartes_revelees; ++i) {
+    for (int i = 0; i < max_cartes_revelees; i++) {
         delete cartes_dehors[i];
     }
     delete [] cartes_dans_pioche;
@@ -24,33 +29,50 @@ Pioche::~Pioche(){
 }
 
 const CarteJoaillerie * Pioche::joueurPrend(int numero_carte) {
-    /*int i = 0;
-    while(cartes_dehors[i]->carte != numero_carte)
-        i++;*/
-    const CarteJoaillerie * temp = cartes_dehors[numero_carte];
+    int i = 0;
+    while(cartes_dehors[i]->getID() != numero_carte) {
+        std::cout << cartes_dehors[i]->getID() << "\n";
+        i++;
+    }
+    std::cout << "Passe\n";
+    const CarteJoaillerie * temp = cartes_dehors[i];
     if (max_cartes_pioche != -1) {
-        cartes_dehors[numero_carte] = cartes_dans_pioche[max_cartes_pioche-1];
+        std::srand(std::time(0));
+        int nombreAleatoire = std::rand() % max_cartes_pioche;
+        cartes_dehors[i] = cartes_dans_pioche[nombreAleatoire];
+        cartes_dans_pioche[nombreAleatoire] = cartes_dans_pioche[max_cartes_pioche-1];
         max_cartes_pioche--;
     }
-    return temp;
-}
-
-// Added by Timo
-const CarteJoaillerie * Pioche::joueurPioche() {
-    // Si pioche vide on sort
-    if (max_cartes_pioche == 0) return nullptr;
-
-    const CarteJoaillerie * temp;
-    temp = cartes_dans_pioche[max_cartes_pioche-1];
-    max_cartes_pioche--;
     return temp;
 }
 
 void Pioche::distribution(){ // ne servira que pour la création de partie
     int i = 0;
     while (i < max_cartes_revelees && max_cartes_pioche != -1) {
-        cartes_dehors[i] = cartes_dans_pioche[max_cartes_pioche-1];
+        std::srand(std::time(0));
+        int nombreAleatoire = std::rand() % max_cartes_pioche;
+        cartes_dehors[i] = cartes_dans_pioche[nombreAleatoire];
+        cartes_dans_pioche[nombreAleatoire] = cartes_dans_pioche[max_cartes_pioche-1];
         max_cartes_pioche--;
         i++;
     }
+}
+
+const CarteJoaillerie * Pioche::joueurPrendPioche() {
+    std::srand(std::time(0));
+    int nombreAleatoire = std::rand() % max_cartes_pioche;
+    const CarteJoaillerie * temp = cartes_dans_pioche[nombreAleatoire];
+    cartes_dans_pioche[nombreAleatoire] = cartes_dans_pioche[max_cartes_pioche-1];
+    max_cartes_pioche--;
+    return temp;
+}
+
+void Pioche::afficherPioche(std::ostream& f){
+    f << "Cartes dans pioche :\n";
+    for (int i = 0; i < max_cartes_pioche; i++)
+        f << "Carte : " << cartes_dans_pioche[i]->getID() << "\n";
+    f << "\n\nCartes revelees :\n";
+    for (int i = 0; i < max_cartes_revelees; i++)
+        f << "Carte : " << cartes_dehors[i]->getID() << "\n";
+    f << "\n\n";
 }
