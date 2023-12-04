067de6cdc0e70502d45bc5eef62c2c1151df56aa
ZhenyangXU <1643013085@qq.com>
2023-12-04 10:30:32 +0100

    Init Cartes dans jeu

--- include/jeu.h
+++ include/jeu.h
@@ -13,6 +13,9 @@
 #include "obligatoire.h"
 #include "joueur.h"
 
+#include <sqlite3.h>
+#include "query.h"
+
 
 class Jeu {
 private:
@@ -53,5 +56,35 @@ public:
     void manche(Plateau * p, Pioche * p1, Pioche * p2, Pioche * p3, Joueur * j1, Joueur * j2, Obligatoire * obl, Optionnelle * opt);
     void tour(Plateau * p, Pioche * p1, Pioche * p2, Pioche * p3, Joueur * j, Obligatoire * obl, Optionnelle * opt);
 };
+
+void initCarteJoaillerie(sqlite3* db, std::vector<CarteJoaillerie>& cartes) {
+    for (int id = 1; id <= 67; ++id) {
+        CarteJoaillerieData data = queryCarteJoaillerie(db, id);
+
+        // 将查询到的数据转换为所需的格式
+        std::array<Couleur, 2> pierres = {static_cast<Couleur>(data.pierres[0]), static_cast<Couleur>(data.pierres[1])};
+        std::map<Couleur, int> prix;
+        for (const auto& p : data.prix) {
+            prix[static_cast<Couleur>(p.first)] = p.second;
+        }
+
+        CarteJoaillerie carte(nullptr, nullptr, data.niveau, data.couronnes, pierres, prix, "", data.pointPrestige, static_cast<Pouvoir>(data.pouvoirs[0]), static_cast<Pouvoir>(data.pouvoirs[1]), id);
+
+        cartes.push_back(carte);
+    }
+}
+
+void initCarteNoble(sqlite3* db, std::vector<CarteNoble>& cartesNoble) {
+    for (int id = 67; id <= 71; ++id) {
+        CarteNobleData data = queryCarteNoble(db, id);
+
+        CarteNoble carteNoble(data.couronnes, data.pointPrestige, {});
+
+        cartesNoble.push_back(carteNoble);
+    }
+}
+
+
+
 
 #endif //JEU_JEU_H
