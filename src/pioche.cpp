6f232d7666f4fe42d77d26f487f18465a1c0102a
Elisa Robeau <elisa.robeau@etu.utc.fr>
2023-12-09 08:23:56 +0000

    Update pioche.cpp

--- src/pioche.cpp
+++ src/pioche.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 //#include "pioche.h"
-#include "jeu.h"
+#include "../include/jeu.h"
 
 Pioche::Pioche(int np, int mcr, int mcp):numero_pioche(np),
                                          max_cartes_revelees(mcr), max_cartes_pioche(mcp){
