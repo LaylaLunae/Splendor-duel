419b4fceb4862247b3bccadad6e03b8a8447f7dd
Marie-anaïs Chabal <marie-anais.chabal@etu.utc.fr>
2023-12-11 20:34:34 +0100

    déguggage pour prise de jetons

--- main.cpp
+++ main.cpp
@@ -64,14 +64,14 @@ int main(int argc, char *argv[]) {
 // test pour prendre des jetons à l'IA :
 
 // pour 2 jetons
-
+/*
     std::vector<std::pair<const Jeton*, const Jeton*>> combinaisons = ia.genererCombinaisonsDeuxJetons(plateau_jeu);
 
     std::cout << "Combinaisons générées :\n";
     for (const auto& combinaison : combinaisons) {
         std::cout << "Jeton 1 : " << combinaison.first->getCouleurString() << ", Jeton 2 : " << combinaison.second->getCouleurString() << std::endl;
-        std::cout<<"tour suivant \n";
     }
+*/
 /*
 // pour 3 jetons
     std::vector<std::tuple<const Jeton*, const Jeton*, const Jeton*>> combinaisons3 = ia.genererCombinaisonsTroisJetons(reinterpret_cast<Plateau *>(&plateau_jeu));
@@ -87,12 +87,12 @@ int main(int argc, char *argv[]) {
         std::cout<<"jslkdfj\n";
     }
 
-    // prendre 1 jeton
-    std::cout<<"fmdlkf \n";
-    ia.choisirJetonSurPlateau(plateau_jeu);
-//    ia.choisirJetonSurPlateau(reinterpret_cast<Plateau *>(&plateau_jeu));
-    std::cout<<"fmdlkf \n";
 */
+    // prendre 1 jeton
+
+    //ia.choisirJetonSurPlateau(plateau_jeu);
+
+
     // tous choix jetons confondus :
    // ia.prendreJetons(reinterpret_cast<Plateau *>(&plateau_jeu));
 
