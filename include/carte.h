183aad09e9314f85952ce9b517e3345c62f9e031
ZhenyangXU <1643013085@qq.com>
2023-12-18 21:59:23 +0100

    Suppression du Plateau* plateau et Joueur* joueur dans Carte et mise dans tous les ActinoPouvoir

--- include/carte.h
+++ include/carte.h
@@ -26,20 +26,20 @@ protected:
     int point_prestige;
     int courronnes;
     std::array<Pouvoir, 2> pouvoirs;
-    Plateau* plateau;
-    Joueur* joueur;
-    Privilege prendrePrivilege(); // take 1 privilege. If none are available, take 1 from your opponent.
-    bool volerJeton(int couleurIndex); // take 1 Gem or Pearl token from your opponent. If your opponent has no such tokens, ignore this effect. You can't take a Gold token from your opponent.
-    bool prendreJeton(unsigned int position_x, unsigned int position_y); // take 1 Token matching the color of this card from the board. If tere are no such tokens left, ignore this effect.
-    void rejouer(); // take antorher turn immediately after ths one ends.
-    void rajouteBonus(int couleurIndex); // Place this card so that it overlaps a Jewel card with a bonus.
+//    Plateau* plateau;
+//    Joueur* joueur;
+    Privilege prendrePrivilege(Plateau* plateau, Joueur* joueur); // take 1 privilege. If none are available, take 1 from your opponent.
+    bool volerJeton(int couleurIndex, Joueur* joueur); // take 1 Gem or Pearl token from your opponent. If your opponent has no such tokens, ignore this effect. You can't take a Gold token from your opponent.
+    bool prendreJeton(unsigned int position_x, unsigned int position_y, Plateau* plateau, Joueur* joueur); // take 1 Token matching the color of this card from the board. If tere are no such tokens left, ignore this effect.
+    void rejouer(Joueur* joueur); // take antorher turn immediately after ths one ends.
+    void rajouteBonus(int couleurIndex, Joueur* joueur); // Place this card so that it overlaps a Jewel card with a bonus.
 
 public:
     Carte(const std::string& chemin, int prestige, int cour, Pouvoir pvr1, Pouvoir pvr2, Plateau* p, Joueur* j);
     Carte();
     virtual ~Carte() {}
 
-    void actionPouvoir();
+    void actionPouvoir(Plateau* plateau, Joueur* joueur);
     int getPointsPrestige();
     int getCourronnes();
     std::array<Pouvoir, 2> existancePouvoir();
--- src/carte.cpp
+++ src/carte.cpp
@@ -4,7 +4,7 @@
 //#include <bits/stdc++.h>
 
 Carte::Carte(const std::string& chemin, int prestige, int cour, Pouvoir pvr1, Pouvoir pvr2, Plateau* p, Joueur* j)
-        : chemin_vers_image(chemin), point_prestige(prestige), courronnes(cour), pouvoirs({pvr1, pvr2}), plateau(p), joueur(j) {
+        : chemin_vers_image(chemin), point_prestige(prestige), courronnes(cour), pouvoirs({pvr1, pvr2}) {
 }
 
 Carte::Carte(){}
@@ -17,7 +17,8 @@ int Carte::getCourronnes() {
     return courronnes;
 }
 
-void Carte::actionPouvoir() {
+// un pointeur sur plateau et un pointeur sur joueur
+void Carte::actionPouvoir(Plateau* plateau, Joueur* joueur) {
     for (const auto& pouvoir : pouvoirs) {
         std::string couleurChoisie;
         Couleur couleur;
@@ -26,7 +27,7 @@ void Carte::actionPouvoir() {
 
         switch (pouvoir) {
             case Pouvoir::nouveau_tour:
-                rejouer();
+                rejouer(joueur);
                 break;
             case Pouvoir::bonus_pierre:
                 while (true) {
@@ -43,17 +44,17 @@ void Carte::actionPouvoir() {
                         std::cout << "Couleur invalide. Veuillez réessayer." << std::endl;
                     }
                 }
-                rajouteBonus(static_cast<int>(couleur));
+                rajouteBonus(static_cast<int>(couleur), joueur);
                 break;
             case Pouvoir::pierre_en_plus:
                 std::cout << "Quelle couleur de jeton voulez-vous voler? Sélectionner l'abscisse x et l'ordonnée y du plateau" << std::endl;
                 int x,y;
                 std::cin >> x;
                 std::cin >> y;
-                prendreJeton(x,y);
+                prendreJeton(x,y,plateau,joueur);
                 break;
             case Pouvoir::privilege_en_plus:
-                prendrePrivilege();
+                prendrePrivilege(plateau, joueur);
                 break;
             case Pouvoir::vol_pierre:
                 while (true) {
@@ -70,7 +71,7 @@ void Carte::actionPouvoir() {
                         std::cout << "Couleur invalide. Veuillez réessayer." << std::endl;
                     }
                 }
-                volerJeton(static_cast<int>(static_cast<int>(couleur)));
+                volerJeton(static_cast<int>(static_cast<int>(couleur)), joueur);
                 break;
             case Pouvoir::rien:
             default:
@@ -83,7 +84,7 @@ std::array<Pouvoir, 2> Carte::existancePouvoir() {
     return this->pouvoirs;
 }
 
-Privilege Carte::prendrePrivilege() {
+Privilege Carte::prendrePrivilege(Plateau* plateau, Joueur* joueur) {
     // Essayez d'obtenir le privilège du Plateau.
     // 1. Si le Plateau a un Privilège disponible, le Joueur actuel appelle ajouterPrivilege() pour l'ajouter à sa propre liste de privilèges.
     // 2. S'il n'y a pas de Privilège disponible sur le Plateau, le Joueur actuel doit en prendre un au Privilège de l'adversaire (si celui-ci en a un).
@@ -99,30 +100,30 @@ Privilege Carte::prendrePrivilege() {
         return privilegeTemp;
     } else {
         // Il n'y a aucun privilege sur le plateau, essayez d'en prendre un à adversaire
-        if (this->joueur->getAdversaire()->hasPrivilege()) { // 检查对手是否有特权
-            Privilege oppPrivilege = this->joueur->getAdversaire()->removePrivilege();
-            this->joueur->ajouterPrivilege(&oppPrivilege);
+        if (joueur->getAdversaire()->hasPrivilege()) { // 检查对手是否有特权
+            Privilege oppPrivilege = joueur->getAdversaire()->removePrivilege();
+            joueur->ajouterPrivilege(&oppPrivilege);
         }
     }
 }
 
 
 // Implémentation de la méthode volerJeton dans la classe Carte
-bool Carte::volerJeton(int couleurIndex) {
+bool Carte::volerJeton(int couleurIndex, Joueur* joueur) {
     // Vérifiez que le joueur et son adversaire sont bien définis
-    if (this->joueur == nullptr || this->joueur->getAdversaire() == nullptr) {
+    if (joueur == nullptr || joueur->getAdversaire() == nullptr) {
         std::cerr << "Les joueurs ne sont pas initialisés ou n'existent pas." << std::endl;
         return false;
     }
 
-    Joueur* adversaire = this->joueur->getAdversaire();
+    Joueur* adversaire = joueur->getAdversaire();
 
     // Vérifiez que l'adversaire a des ressources de la couleur spécifiée
     if (adversaire->getPointsPrestigeCouleurs(couleurIndex) > 0) {
         // Soustraire une ressource de l'adversaire
         adversaire->setPointsPrestigeCouleurs(couleurIndex, adversaire->getPointsPrestigeCouleurs(couleurIndex) - 1);
         // Ajouter une ressource au joueur actuel
-        this->joueur->setPointsPrestigeCouleurs(couleurIndex, this->joueur->getPointsPrestigeCouleurs(couleurIndex) + 1);
+        joueur->setPointsPrestigeCouleurs(couleurIndex, joueur->getPointsPrestigeCouleurs(couleurIndex) + 1);
 
         return true;
     }
@@ -132,21 +133,21 @@ bool Carte::volerJeton(int couleurIndex) {
 }
 
 
-bool Carte::prendreJeton(unsigned int position_x, unsigned int position_y) {
+bool Carte::prendreJeton(unsigned int position_x, unsigned int position_y, Plateau* plateau, Joueur* joueur) {
     if (plateau->hasJeton()) {
         const Jeton* j = plateau->prendreJeton(position_x, position_y);
         int couleurIndex = static_cast<int>(j->getCouleur());
-        joueur->setPointsPrestigeCouleurs(couleurIndex, this->joueur->getPointsPrestigeCouleurs(couleurIndex) + 1); // Add one token of the specified color to the player's inventory
+        joueur->setPointsPrestigeCouleurs(couleurIndex, joueur->getPointsPrestigeCouleurs(couleurIndex) + 1); // Add one token of the specified color to the player's inventory
             return true;
     }
     return false;
 }
 
-void Carte::rejouer() {
-    this->joueur->resetRejouer(true);
-    this->joueur->getAdversaire()->resetRejouer(false);
+void Carte::rejouer(Joueur* joueur) {
+    joueur->resetRejouer(true);
+    joueur->getAdversaire()->resetRejouer(false);
 }
 
-void Carte::rajouteBonus(int couleurIndex) {
-    this->joueur->setGemmesBonus(couleurIndex,this->joueur->getGemmesBonus(couleurIndex) + 1);
+void Carte::rajouteBonus(int couleurIndex, Joueur* joueur) {
+    joueur->setGemmesBonus(couleurIndex,joueur->getGemmesBonus(couleurIndex) + 1);
 }
