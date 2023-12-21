9ac36ee19c367567d6092aed35bf4cae8905df78
Marie-anaïs Chabal <marie-anais.chabal@etu.utc.fr>
2023-12-21 17:10:06 +0100

    IA cpp commenté

--- src/IA.cpp
+++ src/IA.cpp
@@ -7,7 +7,7 @@
 #include <random>
 
 class Plateau;
-
+// cette fonction permet à l'IA de faire tous ces choix de manière aléatoire
 int IA::choisirChoix(int min, int max) {
     int choix = rand() % max + 1;
 
@@ -16,29 +16,31 @@ int IA::choisirChoix(int min, int max) {
     }
     return choix;
 }
+//Cette fonction permet de mettre dans un vecteur toutes les cartes que l'IA peut acheter en fonction des ressources qu'elle possède.
+//Cette fonction utilise les fonctions de la classe obligatoire afin de ne pas re-coder deux fois la même fonction.
 
 std::vector<CarteJoaillerie*> IA::getCartesAchetable(const Pioche& pioche) const {
     std::vector<CarteJoaillerie*> cartesAchetable;
-
-    for (int i = 0; i < pioche.getMaxCartesRevelees(); ++i) {
+    for (int i = 0; i < pioche.getMaxCartesRevelees(); ++i) {   // parcourt toutes les cartes disponibles
         const CarteJoaillerie* carte = pioche.getCartesDehors(i);
-
         if (carte) {
             std::vector<int> difference = Obligatoire::calculDifference((Joueur *) this, carte->getPrix());
-
             if (Obligatoire::achatPiochePossible((Joueur *) this, const_cast<Pioche*>(&pioche))) {
-                cartesAchetable.push_back(const_cast<CarteJoaillerie*>(carte));
+                cartesAchetable.push_back(const_cast<CarteJoaillerie*>(carte));  // on ajout dans le vecteur si l'IA peut acheter la carte
             }
         }
     }
     return cartesAchetable;
 }
 
+//Cette fonction récupère le vecteur qui contient toutes les cartes que l'IA peut acheter, mélange le vectoeur et retroune la dernière carte
+// Cette denrière carte sera ensuite acheter par l'utilisateur, les ressources correspondantes remises dans le sac, les bonus et les pouvoirs activés et ajouté au joueur.
+
 CarteJoaillerie* IA::melangerEtObtenirDerniereCarte(Pioche& pioche, Joueur& joueur, Plateau& plateau) {
     if (pioche.getMaxCartesRevelees() == 0 || pioche.getCartesDehors(0) == nullptr) {
         return nullptr;
     }
-    std::random_device rd;
+    std::random_device rd;  // cela permet de créer le génèrateur pour mélanger ensuite le vecteur
     std::default_random_engine generator(rd());
 
     std::vector<CarteJoaillerie*> cartesAchetable = getCartesAchetable(pioche);
@@ -46,21 +48,24 @@ CarteJoaillerie* IA::melangerEtObtenirDerniereCarte(Pioche& pioche, Joueur& joue
         return nullptr;
     }
     std::shuffle(cartesAchetable.begin(), cartesAchetable.end(), generator);
-    CarteJoaillerie* derniereCarte = cartesAchetable.back();
+    CarteJoaillerie* derniereCarte = cartesAchetable.back();    // on retir la carte du vecteur
 
     for (const auto& cout : derniereCarte->getPrix()) {
         if (cout.second > 0) {
+            // on remet les jetons utilisés dans le sac
             Obligatoire::remettreJetonSac(&joueur, &plateau, cout.first, joueur.getNbJeton((int) cout.first) - cout.second, cout.second,(int) cout.first);
         }
     }
-    joueur.ajouterCarteJoaillerie(*derniereCarte);
+    joueur.ajouterCarteJoaillerie(*derniereCarte);  // on ajoute à la main du joueur les bonus
 
     auto* carte_non_const = const_cast<CarteJoaillerie*>(derniereCarte);
     carte_non_const->actionPouvoir(&plateau, &joueur);
-
     return derniereCarte;
 }
 
+//Cette fonction met dans un vecteur toutes les combinaisons de deux jetons que l'IA à le droit de prendre.
+// Ce vecteur est ensuite mélanger
+// la dernier combinaison est ensuite retirer du vecteur, cette combinaison correspond à celle que va prendre l'IA.
 std::vector<std::pair<const Jeton*, const Jeton*>> IA::genererCombinaisonsDeuxJetons(Plateau* plateau) {
     std::vector<std::pair<const Jeton*, const Jeton*>> combinaisons;
 
@@ -71,7 +76,7 @@ std::vector<std::pair<const Jeton*, const Jeton*>> IA::genererCombinaisonsDeuxJe
         // Obtenir les positions possibles pour le jeton1
         unsigned int x = i / 5;
         unsigned int y = i % 5;
-
+// on utilise la fonction de plateau pour avoir toutes les combinaisons possible pour un jeton donné
         std::vector<std::vector<unsigned int>> positionsPossibles = std::vector<std::vector<unsigned int>>(0);
         for (auto vec : plateau->donnePositionsPossiblesAPartirDe(x, y)) {
             positionsPossibles.push_back(vec);
@@ -92,22 +97,25 @@ std::vector<std::pair<const Jeton*, const Jeton*>> IA::genererCombinaisonsDeuxJe
             unsigned int posX = positions[k].first;
             unsigned int posY = positions[k].second;
             const Jeton* jeton2 = plateau->getJeton(posX * 5 + posY);
-
+// on utilise la fonction de plateau afin de faire la selection
             int selectionResult = plateau->selectionJeton(posX, posY);
             combinaisons.push_back({jeton1, jeton2});
-
+// Si la selection possède de l'or, on recommence.
             if(selectionResult==2){
                 return genererCombinaisonsDeuxJetons(plateau);
             }
+            // s'il y a deux perles l'adeversaire prend un privilège.
             if (jeton1->getCouleur()==Couleur::rose && jeton2->getCouleur()==Couleur::rose) {
                 Obligatoire::donnerPrivilegeAdversaire(this, plateau);
             }
         }
     }
+    // on utilise la fonciton de plateau afin de valider la selection en cours.
     plateau->validerSelectionEtPrendreJetons();
     return combinaisons;
 }
 
+// Cette fonction est la même que la fonction précédente mais cetet fois-ci avec une combinaison de 3 jetons au lieu de 2.
 std::vector<std::tuple<const Jeton*, const Jeton*, const Jeton*>> IA::genererCombinaisonsTroisJetons(Plateau* plateau) const {
     std::vector<std::tuple<const Jeton *, const Jeton *, const Jeton *>> combinaisons;
     // Parcourir le plateau de jetons
@@ -160,6 +168,8 @@ std::vector<std::tuple<const Jeton*, const Jeton*, const Jeton*>> IA::genererCom
     return combinaisons;
 }
 
+// Cette fonction peremt à l'IA de choisir aléatoirement si elle souahite prendre 1, 2, ou 3 jetons.
+// Cela est fait en fonction du nombre de jetons qu'elle possède et à l'aide de la fonction choisirChoix.
 void IA::prendreJetons(Plateau* plateau) {
     if (!plateau->hasJeton()) {
         Optionnelle::remplissagePlateau(this, plateau);
@@ -167,10 +177,11 @@ void IA::prendreJetons(Plateau* plateau) {
     // Calculer le nombre actuel de jetons
     int nombreJetonsActuel = std::accumulate(nb_jeton.begin(), nb_jeton.end(), 0);
     int choix;
-
+// Si l'IA a déjà 9 jetons elle ne peut pas en prendre plus
     if (nombreJetonsActuel == 9) {
         choisirJetonSurPlateau(plateau);
     }
+    // Si l'IA a déjà 8 jetons elle peut prendre soit 1 jetons soit 2 jetons mais pas 3.
     if (nombreJetonsActuel == 8) {
         choix = choisirChoix(1, 2);
         if (choix == 1)
@@ -180,6 +191,7 @@ void IA::prendreJetons(Plateau* plateau) {
             std::vector<int> resultatAjout = Obligatoire::ajouterJetonsJoueur(this, {combinaisonsDeux[0].first, combinaisonsDeux[0].second});
         }
     }
+    // SI l'IA a 7 ou moins de jetons elle peut en prendre autant qu'elle souhaite.
     else {
         choix = choisirChoix(1, 3);
 
@@ -198,6 +210,7 @@ void IA::prendreJetons(Plateau* plateau) {
     }
 }
 
+// Cette fonction permet à l'IA de prendre qu'un seul jetons disponible sur le plateau.
 void IA::choisirJetonSurPlateau(Plateau* plateau) {
     int positionX = rand() % 5 ;
     int positionY = rand() % 5 ;
@@ -210,6 +223,6 @@ void IA::choisirJetonSurPlateau(Plateau* plateau) {
         std::vector<int> resultatsAjout = Obligatoire::ajouterJetonsJoueur(this, jetonsSelectionnes);
     }
     else
-        // clear la selection
+        // Dans le cas ou la selection n'est pas valide, comme par exmeple si le jeton choisi en premier lieu est un or on rappelle la fonction.
         choisirJetonSurPlateau(plateau);
 }
